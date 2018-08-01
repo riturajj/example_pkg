1c993d0994574bbfb1a5c5ec02e69bcd33410b3e
Rituraj Jagtap <39925233+riturajj@users.noreply.github.com>
2018-08-01 11:25:06 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example_pkg",
+    name="example_pkg_riturajjtest",
     version="0.0.1",
     author="Rituraj Jagtap",
     author_email="riturajj@google.com",
     description="A small example package",
     long_description=long_description,
-    url="https://github.com/riturajj/example_pkg",
+    url="https://github.com/riturajj/example_pkg_riturajjtest",
     packages=setuptools.find_packages()
 )
